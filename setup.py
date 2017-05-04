9226a355e09eb6a5d0d8b871710d4423bdfef43a
Lotta Mether <mether@castor.(none)>
2017-05-04 17:33:43 +0200

    Added numpy to include list
    It seems to be needed on some systems...

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize([Extension("klu", ["klu.pyx", 'klu_interf.c'], 
+    ext_modules = cythonize([Extension("klu", ["klu.pyx", 'klu_interf.c'], include_dirs=[numpy.get_include()],
 								libraries=[	'klu', 
 											'btf', 
 											'amd', 
